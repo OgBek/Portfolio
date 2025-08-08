a4b09ce54917e012902fc9a810891e57291508e0
OgBek <bbekam60@gmail.com>
2025-08-08 18:05:31 +0300

    Update About.tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import _React from 'react';
 import { motion } from 'framer-motion';
 import { FiAward, FiBriefcase, FiCode } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
